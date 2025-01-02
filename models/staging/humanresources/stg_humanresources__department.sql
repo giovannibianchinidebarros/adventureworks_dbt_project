3d9a315075783f583d228049be073dd29db82576
Giovanni Barros <giovanni.bianchini.lh2024@gmail.com>
2025-01-02 20:15:27 +0000

    remove example folder, create stg and dim_department

---
+++ models/staging/humanresources/stg_humanresources__department.sql
@@ -0,0 +1,11 @@
+WITH base AS (
+  SELECT
+    JSON_QUERY(data, '$') AS json_data
+  FROM `adventureworks-project.adventureworks_dataset.humanresources_department`
+)
+SELECT
+  JSON_VALUE(json_data, '$.departmentid') AS department_id,
+  JSON_VALUE(json_data, '$.name') AS name,
+  JSON_VALUE(json_data, '$.groupname') AS group_name,
+  JSON_VALUE(json_data, '$.modifieddate') AS modified_date
+FROM base
