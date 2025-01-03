2e561ce5b46382204bfda9f0553198c852942bcf
giovanni bianchini <giovanni.bianchini.lh2024@gmail.com>
2025-01-03 02:15:10 +0000

    add staging for all humanresources tables

--- models/staging/humanresources/stg_humanresources__department.sql
+++ models/staging/humanresources/stg_humanresources__department.sql
@@ -1,11 +1,11 @@
 WITH base AS (
-  SELECT
-    JSON_QUERY(data, '$') AS json_data
-    from {{ source('humanresources','humanresources_department') }}
+    SELECT
+        JSON_QUERY(data, '$') AS json_data
+    FROM {{ source('humanresources','humanresources_department') }}
 )
 SELECT
-  JSON_VALUE(json_data, '$.departmentid') AS department_id,
-  JSON_VALUE(json_data, '$.name') AS name,
-  JSON_VALUE(json_data, '$.groupname') AS group_name,
-  JSON_VALUE(json_data, '$.modifieddate') AS modified_date
+    SAFE_CAST(JSON_VALUE(json_data, '$.departmentid') AS INT64) AS department_id,
+    JSON_VALUE(json_data, '$.name') AS name,
+    JSON_VALUE(json_data, '$.groupname') AS group_name,
+    SAFE_CAST(JSON_VALUE(json_data, '$.modifieddate') AS DATETIME) AS modified_date
 FROM base
---
+++ models/staging/humanresources/stg_humanresources__employeedepartmenthistory.sql
@@ -0,0 +1,13 @@
+WITH base AS (
+  SELECT
+    JSON_QUERY(data, '$') AS json_data
+  FROM {{ source('humanresources', 'humanresources_employeedepartmenthistory') }}
+)
+SELECT
+  SAFE_CAST(JSON_VALUE(json_data, '$.businessentityid') AS INT64) AS business_entity_id,
+  SAFE_CAST(JSON_VALUE(json_data, '$.departmentid') AS INT64) AS department_id,
+  SAFE_CAST(JSON_VALUE(json_data, '$.shiftid') AS INT64) AS shift_id,
+  SAFE_CAST(JSON_VALUE(json_data, '$.startdate') AS DATE) AS start_date,
+  SAFE_CAST(JSON_VALUE(json_data, '$.enddate') AS DATE) AS end_date,
+  SAFE_CAST(JSON_VALUE(json_data, '$.modifieddate') AS DATETIME) AS modified_date
+FROM base
