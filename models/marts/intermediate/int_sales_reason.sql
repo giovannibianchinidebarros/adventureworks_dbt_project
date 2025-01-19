9a31df92b1c59414c298a320be77bd286d5be416
Giovanni Barros <giovanni.barros.lh2024@gmail.com>
2025-01-19 02:48:36 +0000

    create fct_sales

--- models/marts/intermediate/int_sales_reason.sql
+++ models/marts/intermediate/int_sales_reason.sql
@@ -20,25 +20,3 @@ sales_reason AS (
 )
 SELECT * FROM final
 order by sales_order_id
-
-
-
--- WITH 
--- subcategory AS (
---     SELECT * 
---     FROM {{ ref("stg_production__product_subcategory") }}
--- )
--- , category AS (
---     SELECT * 
---     FROM {{ ref("stg_production__product_category") }}
--- )
--- , final AS (
---     SELECT
---         subcategory.pk_product_subcategory_id
---         , subcategory.subcategory_name
---         , category.category_name
---     FROM subcategory
---     LEFT JOIN category 
---         ON subcategory.fk_product_category_id = category.pk_product_category_id
--- )
--- SELECT * FROM final
