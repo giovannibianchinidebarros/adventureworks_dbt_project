828c291f911bcbfdca4fbe6c8be7e82fc7bbb966
giovanni barros <giovanni.barros.lh2024@gmail.com>
2025-01-19 19:32:49 +0000

    update fct_sales, create fct_sales_items

--- models/marts/dim/dim_sales_person.sql
+++ models/marts/dim/dim_sales_person.sql
@@ -3,9 +3,14 @@ sales_person AS (
     SELECT * 
     FROM {{ ref("stg_sales__sales_person") }}
 )
+, person AS (
+    SELECT * 
+    FROM {{ ref("int_person") }}
+)
 , final AS (
     SELECT
         sales_person.pk_salesperson_id
+        , person.person_name as sales_person_name
         , sales_person.fk_territory_id
         , sales_person.sales_quota
         , sales_person.bonus
@@ -14,5 +19,7 @@ sales_person AS (
         , sales_person.sales_last_year
 
     FROM sales_person
+    LEFT JOIN person 
+        ON sales_person.pk_salesperson_id = person.person_id
 )
 SELECT * FROM final
---
+++ models/marts/fct/fct_sales_items.sql
@@ -0,0 +1,58 @@
+WITH 
+item AS (
+    SELECT * 
+    FROM {{ ref("stg_sales__sales_order_detail") }}
+)
+, sales AS (
+    SELECT * 
+    FROM {{ ref("stg_sales__sales_order_header") }}
+)
+, product AS (
+    SELECT *
+    FROM {{ ref("dim_product") }}
+)
+, territory AS (
+    SELECT *
+    FROM {{ ref("dim_territory") }}
+)
+, date AS (
+    SELECT * 
+    FROM {{ ref("dim_date") }}
+)
+, final AS (
+    SELECT
+        item.fk_sales_order_id as sales_order_id
+        , item.pk_sales_order_detail_id as sales_order_detail_id
+        , item.order_quantity
+        , item.fk_product_id
+        , item.unit_price
+        , sales.order_date
+        , date.year_number as order_date_year
+        , date.month_of_year as order_date_month
+        , date.month_name as order_date_month_name
+        , sales.fk_territory_id
+        , territory.pk_territory_id as territory_id
+        , territory.territory_name
+        , territory.country_region_code
+        , territory.country_region_name
+        , territory.territory_group
+        , product.product_id
+        , product.product_name
+        , product.standard_cost
+        , product.list_price
+        , product.subcategory
+        , product.category
+        , product.model_name
+
+    FROM item
+    LEFT JOIN sales 
+        ON item.fk_sales_order_id = sales.pk_sales_order_id
+    LEFT JOIN product 
+        ON item.fk_product_id = product.product_id
+    LEFT JOIN territory 
+        ON sales.fk_territory_id = territory.pk_territory_id
+    LEFT JOIN date 
+        ON sales.order_date = date.date_day
+)
+SELECT * FROM final
+ORDER BY sales_order_id, sales_order_detail_id
